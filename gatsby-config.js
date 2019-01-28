027bc72b641bc589d74985b85cb1434fcb92be8e
Richard Torcato <rtorcato@me.com>
2019-01-28 10:28:23 -0500

    changes to image path

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/static/img`,
+        path: `${__dirname}/static/media`,
         name: 'uploads',
       },
     },
